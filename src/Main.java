7782c5258508dbbd113458ec384245c78b1d6087
brian070069 <brianwgatundu@gmail.com>
2024-07-08 16:22:03 +0300

    first commit on classes

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,13 @@
 public class Main {
     public static void main(String[] args) {
+<<<<<<< HEAD
         System.out.println("Hello world!");
+=======
+        Car car = new Car();
+        car.describeCar();
+        System.out.println("make ="+car.getMake());
+        System.out.println("model ="+car.getModel());
+        car.setMake("Isuzu");
+>>>>>>> 27a98e0 (first commit on classes)
     }
 }
